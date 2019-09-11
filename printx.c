db24ff744d452acad0957fbe86c82652d84de332
eusougz <guilhermegms209@gmail.com>
2019-09-11 10:21:35 -0300

    tsl-solution

--- printx.c
+++ printx.c
@@ -7,14 +7,21 @@
 #define NUM_THREADS 5
 
 int x = 0 ;
+volatile int lock = 0;
 
 void *threadBody (void *id)
 {
-   long tid = (long) id ;
+    long tid = (long) id ;
 	int i;
-	for (i=0;i<10000;i++)
-	   x++ ;
-   pthread_exit (NULL) ;
+	
+	for (i=0;i<10000;i++) {
+		while(__sync_lock_test_and_set(&lock, 1));
+		////// SEÇÃO CRÍTICA //////
+		x++ ;
+		///////////////////////////
+		__sync_lock_release(&lock);
+	}
+	pthread_exit (NULL);
 }
 
 int main (int argc, char *argv[])
@@ -23,14 +30,15 @@ int main (int argc, char *argv[])
    long i, status ;
    
    for (i=0; i<NUM_THREADS; i++) {
-      status = pthread_create (&thread[i], NULL, threadBody, (void *) i) ;
+		status = pthread_create (&thread[i], NULL, threadBody, (void *) i) ;
 //		sleep(1);
 		CHECK(status==0);
    }
 	for (i=0; i<NUM_THREADS; i++){
-		   status = pthread_join (thread[i], NULL) ;
-			CHECK(status==0);
+		status = pthread_join (thread[i], NULL) ;
+		CHECK(status==0);
 	}
+
 	printf ("\n\nValor final de x: %02d\n\n", x) ;	
-   pthread_exit (NULL) ;
+    pthread_exit (NULL) ;
 }
